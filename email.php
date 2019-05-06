2c3ac8c17429b33d8add85700b87804b6dccc196
Salvador B <mexidense@gmail.com>
2019-05-06 12:38:48 +0200

    Ready from contact form

--- email.php
+++ email.php
@@ -15,29 +15,41 @@ $dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
 
 try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
-    $mail->isSMTP();                                            // Set mailer to use SMTP
-    $mail->Host       = getenv('HOST_EMAIL');  // Specify main and backup SMTP servers
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = getenv( 'USER_EMAIL');                     // SMTP username
-    $mail->Password   = getenv('PASSWORD_EMAIL');                               // SMTP password
-    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-    $mail->Port       = 587;                                    // TCP port to connect to
+    if (isset($_POST['name']) && isset($_POST[ 'email'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $service = '';
+        $message = '';
 
-    //Recipients
-    $mail->setFrom('from@domain.com', 'Mailer name');
-    $mail->addAddress( 'to@domain.com', 'Receiver name');     // Add a recipient
-    
+        if (isset($_POST['service']) && isset($_POST['message'])) {
+            $service = $_POST['service'];
+            $message = $_POST['message'];
+        }
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Contact from carlaaguero.com';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        //Server settings
+        $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+        $mail->isSMTP();                                            // Set mailer to use SMTP
+        $mail->Host       = getenv('HOST_EMAIL');  // Specify main and backup SMTP servers
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = getenv('USER_EMAIL');                     // SMTP username
+        $mail->Password   = getenv('PASSWORD_EMAIL');                               // SMTP password
+        $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+        $mail->Port       = 587;                                    // TCP port to connect to
 
-    $mail->send();
-    echo 'Message has been sent';
+        //Recipients
+        $mail->setFrom($email, $name);
+        $mail->addAddress('to@example.com', 'Receiver name');     // Add a recipient
+
+
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Contact from host.com about ' . empty($service) ?: 'general topic';
+        $mail->Body    = $message;
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'Message has been sent';
+    }  
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
