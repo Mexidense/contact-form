c0e5e62fa18b0d7a994c8e545e70e5298976a16d
Salvador B <mexidense@gmail.com>
2019-05-06 12:49:43 +0200

    Adding more env variables

--- email.php
+++ email.php
@@ -27,14 +27,14 @@ try {
         }
 
         //Server settings
-        $mail->SMTPDebug = 2;                                       // Enable verbose debug output
-        $mail->isSMTP();                                            // Set mailer to use SMTP
-        $mail->Host       = getenv('HOST_EMAIL');  // Specify main and backup SMTP servers
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = getenv('USER_EMAIL');                     // SMTP username
-        $mail->Password   = getenv('PASSWORD_EMAIL');                               // SMTP password
-        $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-        $mail->Port       = 587;                                    // TCP port to connect to
+        $mail->SMTPDebug = 2;                          // Enable verbose debug output
+        $mail->isSMTP();                               // Set mailer to use SMTP
+        $mail->Host       = getenv('HOST_EMAIL');      // Specify main and backup SMTP servers
+        $mail->SMTPAuth   = true;                      // Enable SMTP authentication
+        $mail->Username   = getenv('USER_EMAIL');      // SMTP username
+        $mail->Password   = getenv('PASSWORD_EMAIL');  // SMTP password
+        $mail->SMTPSecure = getenv('SECURE_EMAIL');    // Enable TLS encryption, `ssl` also accepted
+        $mail->Port       = getenv('PORT_EMAIL');      // TCP port to connect to
 
         //Recipients
         $mail->setFrom($email, $name);
